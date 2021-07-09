5304134556a10031c55922f9a081334014e35c1f
Axun_8bbn <Axun_8bbn@itstep.edu.az>
2021-07-09 12:57:36 +0400

    Add project files.

---
+++ WpfApp1/LoadWin.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for LoadWin.xaml
+    /// </summary>
+    public partial class LoadWin : Window
+    {
+        public ObservableCollection<Car> Cars { get; set; }
+
+        public LoadWin(ObservableCollection<Car> cars)
+        {
+            InitializeComponent();
+            Cars = cars;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Car car = new Car
+            {
+                Vendor = vendor.Text,
+                Model = model.Text,
+                Year = int.Parse(year.Text),
+                IsNew = isNew.Text == "Yes"
+
+            };
+
+            Cars.Add(car);
+
+            System.Windows.MessageBox.Show(Cars[Cars.Count - 1].Vendor);
+
+            this.Close();
+
+            vendor.Text = model.Text = year.Text = isNew.Text = default;
+            
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public ObservableCollection<Car> Cars { get; set; } = new ObservableCollection<Car>();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadWin window = new LoadWin(Cars);
+            window.ShowDialog();         
+        }
+    }
+
+    public class Car
+    {
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public bool IsNew { get; set; }
+    }
+
+}
